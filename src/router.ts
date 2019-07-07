0a8eea6f83ef921f44a6b7ff2676cefbd36b56f9
Arthur Malheiros <arthurgoncalvesmalheiros@gmail.com>
2019-07-07 03:30:04 -0300

    a

---
+++ src/router.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HomeApp from '@/views/HomeApp.vue';
+import LogIn from '@/views/LogIn.vue';
+
+Vue.use(Router);
+const router = new Router({
+  routes: [
+        { 
+        path: '/',
+        name: 'HomeApp',
+        component: HomeApp,
+        },
+        {
+            path: '/LogIn',
+            name: 'LogIn',
+            component: LogIn,
+        }
+    ]
+});
+export default router;
